0aa2ebe45254e484c205633407ad27d25086d0a3
Vatsal Sharma <Vatsalsharma1511@gmail.com>
2026-06-15 12:28:26 +0530

    allow all CORS origins

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ app = FastAPI(title="Vertex DSA Tutor")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
